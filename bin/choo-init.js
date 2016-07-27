304681c771911463845328b56534ef6d425773af
Matt McFarland <contact@mattmcfarland.com>
2016-07-27 07:10:49 -0400

    initial draft (wip)

--- bin/choo-init.js
+++ bin/choo-init.js
@@ -1,3 +1,46 @@
 #!/usr/bin/env node
 
-console.log('choo init')
+/* Dependencies */
+const assert = require('assert')
+const minimist = require('minimist')
+const resolve = require('resolve')
+const helpFile = require('./help')
+const { createConfig, config, destinationPath } = require('../lib/utils')
+const { prop, either, curry, defaultTo, compose } = require('ramda')
+
+const argv = minimist(process.argv.slice(2))
+if (argv.help) {
+  console.log(helpFile.init)
+  process.exit(0)
+}
+
+const defaultTpl = 'template-lite'
+
+// (String, Object) -> String
+const maybeTemplateOrDefault = curry(
+  (s, o) => compose(
+    defaultTo(s),
+    either(prop('template'), prop('t'))
+  )(o)
+)
+const template = maybeTemplateOrDefault(defaultTpl, argv)
+
+const resolveTemplateFromDir = (basedir) => resolve(template, {basedir}, (err, res) => {
+  if (err) {
+    console.error('error', err)
+    process.exit(0)
+  }
+  const runner = require(res)
+  assert(runner.init, 'template must have init export')
+  assert(runner.add, 'template must have add export')
+  const appConfig = runner.init(argv)
+  // Just in case we want to pass vanilla props or use
+  // a promise object.
+  if (appConfig.then) {
+    appConfig.then(props => createConfig(props, template)).then(() => runner.generate(destinationPath, config)).catch(console.log)
+  } else {
+    createConfig(appConfig).then(() => runner.generate(destinationPath, config)).catch(console.log)
+  }
+})
+
+resolveTemplateFromDir(template === defaultTpl ? __dirname : process.cwd())
--- bin/help.js
+++ bin/help.js
@@ -10,55 +10,27 @@ Available commands include:
     init, t  Scaffold a new app in the current working directory.
              Uses the demo template if no options are added.
 
-     add, a  Add a view, element, component, or model template to
-             the current choo app.
+     add, a  Adds template dependent components, models, views, etc.
 
 You can learn more about a specific command by running things like:
 
   choo init --help
-  choo add  --help
+  choo add  --help (depends on the template)
 
 `
 
-exports.choo = `
+exports.init = `
 Scaffold a new app in the current working directory.
 
 Usage: choo-init [options]
 
 Available options include:
 
-  --template, -t  ['@choo/demo-template, @choo/minimal-template']
-                  You can also provide a path to a template.js file
-                  or even an npm repo name.
+  --template, -t  [path-or-npm-module]
+                  You can also provide a path to a supported template library
 
 Additional options:
 
   --help, -h  Shows this.
 
-`
-
-exports.add = `
-Usage: choo-add [options] [args]
-
-Available scaffold options:
-
-        --view, -v  [view-name]
-                    Creates a new view and also adds it to the routes.js
-                    file.  Adds the file to src/views/[view-name].js
-
-       --model, -m  [model-name]
-                    Create a choo.model, saved to src/models/[model-name].js
-
-     --element, -e  [element-name]
-                    Create a choo/html, saved to src/elements/[element-name].js
-
-   --component, -c  [component-name]
-                    Create a choo/html, saved to src/components/[component-name].js
-
-Additional options:
-
-  --path, -p  [path-name]
-              Changes the path where the file will be saved.
-
-  --help, -h  Shows this.
 `
