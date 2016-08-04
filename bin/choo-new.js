ed98aff98dea8e36c2d2d4d931b52d64d84fbe82
Matt McFarland <contact@mattmcfarland.com>
2016-08-04 15:09:11 -0400

    Regarding #1 - Add feedback if user forgets app name
    If user does choo new <enter> they will be prompted for a name.
    tested and working as expected.

--- bin/choo-new.js
+++ bin/choo-new.js
@@ -2,12 +2,21 @@
 
 const resolvePath = require('path').resolve
 const kebabCase = require('lodash').kebabCase
+const inquirer = require('inquirer')
 const appGenerator = require('../generators/app')
 const args = process.argv.slice(2)
-const projectName = kebabCase(args[0])
-const projectPath = resolvePath(process.cwd(), projectName)
-process.env.PROJECT_PATH = projectPath
 
-console.log('Create', projectPath)
-
-appGenerator({projectName})
+if (!args[0]) {
+  inquirer.prompt({
+    type: 'input',
+    name: 'projectName',
+    message: 'What is your project name?'
+  }).then(props => {
+    process.env.PROJECT_PATH = resolvePath(process.cwd(), props.projectName)
+    appGenerator({projectName: props.projectName})
+  })
+} else {
+  const projectName = kebabCase(args[0])
+  process.env.PROJECT_PATH = resolvePath(process.cwd(), projectName)
+  appGenerator({projectName})
+}
