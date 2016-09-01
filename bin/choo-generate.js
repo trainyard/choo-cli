61fa68a0028e97978285abe79e90ba65423a69a8
Yerko Palma <yerko.palma@usach.cl>
2016-09-01 13:32:59 +0000

    allow custom generators for custom templates

--- bin/choo-generate.js
+++ bin/choo-generate.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+const yaml = require('yamljs')
+const fs = require('fs')
+const findConfig = require('../lib/utils').findConfig
 const help = require('./help')
 const _ = require('lodash')
 const args = process.argv.slice(2)
@@ -24,11 +27,11 @@ if (!args[1]) {
 const category = args[0]
 const fileName = _.kebabCase(args[1])
 const name = _.camelCase(args[1])
+const config = yaml.parse(fs.readFileSync(findConfig(), 'utf8'))[category] || {}
 
 try {
-  generators[category]({fileName: fileName, name: name})
+  generators[category]({fileName: fileName, name: name, path: config.path})
 } catch (e) {
   console.log(help.generate)
   throw e
 }
-
