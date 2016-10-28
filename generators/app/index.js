87de12e637630b7dd8c4dccc732e1a2b2b9e6d4c
MattMcFarland <contact@mattmcfarland.com>
2016-10-28 16:23:57 -0400

    decouple generator wip

--- generators/app/index.js
+++ generators/app/index.js
@@ -25,58 +25,59 @@ function formatState (state) {
 }
 module.exports = (props) => {
   const destinationPath = utils.newProjectPath(props.projectName)
+  const templateRepo = (!props.templateRepo) ? 'trainyard/template-basic' : props.templateRepo
 
-  if (!props.templateRepo) {
-    const templatePath = path.join(__dirname, 'template')
-    generateApp(templatePath, destinationPath, props)
-  } else {
-    const repo = `https://github.com/${props.templateRepo}.git`
-    // clone the template repo in the current directory
-    exec('git', ['clone', '-q', repo], {}, function () {
-      // compile template
-      const projectPath = path.join(process.cwd(), props.templateRepo.split('/').pop())
-      // check if choo.yaml exist
-      fs.stat(path.join(projectPath, '_choo.yaml'), (err, stats) => {
-        if (err || !stats.isFile()) {
-          exec('rm', ['-rf', projectPath])
-          console.log(chalk.red.bold('_choo.yaml') + chalk.gray(' file missing in custom template.'))
-          process.exit(1)
-        }
-        // check that the cloned repo has choo as a dependency
-        const pkg = JSON.parse(fs.readFileSync(path.join(projectPath, '_package.json')))
-        if (!pkg.dependencies || !pkg.dependencies.choo) {
-          exec('rm', ['-rf', projectPath])
-          console.log(chalk.red.bold('choo') + chalk.gray(' was not found as a dependency of the custom template'))
-          process.exit(1)
-        }
-        const defaults = {
-          required: ['_choo.yaml']
-        }
-        const config = Object.assign(defaults, yaml.parse(fs.readFileSync(path.join(projectPath, '_choo.yaml'), 'utf8')))
-        // now that choo.yml file is present, check which files are required
-        if (config.required && config.required.length > 0) {
-          config.required.forEach((req, index) => {
-            fs.stat(projectPath + '/' + req, (err, stats) => {
-              if (err || !stats.isFile()) {
-                exec('rm', ['-rf', projectPath])
-                console.log(chalk.red.bold(req) + chalk.gray(' file missing in custom template.'))
-                process.exit(1)
-              } else if (index === config.required.length - 1) {
-                generateApp(projectPath, destinationPath, props, config.required)
-                exec('rm', ['-rf', projectPath])
-              }
-            })
+  const repo = `https://github.com/${templateRepo}.git`
+  // clone the template repo in the current directory
+  exec('git', ['clone', '-q', repo], {}, function () {
+
+    // compile template
+    const projectPath = path.join(process.cwd(), templateRepo.split('/').pop(), '_app')
+
+    // check if choo.yaml exist
+    fs.stat(path.join(projectPath, '_choo.yaml'), (err, stats) => {
+      console.log(projectPath)
+      if (err || !stats.isFile()) {
+        exec('rm', ['-rf', projectPath])
+        console.log(chalk.red.bold('_choo.yaml') + chalk.gray(' file missing in custom template.'))
+        process.exit(1)
+      }
+      // check that the cloned repo has choo as a dependency
+      const pkg = JSON.parse(fs.readFileSync(path.join(projectPath, '_package.json')))
+      if (!pkg.dependencies || !pkg.dependencies.choo) {
+        exec('rm', ['-rf', projectPath])
+        console.log(chalk.red.bold('choo') + chalk.gray(' was not found as a dependency of the custom template'))
+        process.exit(1)
+      }
+      const defaults = {
+        required: ['_choo.yaml']
+      }
+      const config = Object.assign(defaults, yaml.parse(fs.readFileSync(path.join(projectPath, '_choo.yaml'), 'utf8')))
+      // now that choo.yml file is present, check which files are required
+      if (config.required && config.required.length > 0) {
+        config.required.forEach((req, index) => {
+          fs.stat(projectPath + '/' + req, (err, stats) => {
+            if (err || !stats.isFile()) {
+              exec('rm', ['-rf', projectPath])
+              console.log(chalk.red.bold(req) + chalk.gray(' file missing in custom template.'))
+              process.exit(1)
+            } else if (index === config.required.length - 1) {
+              generateApp(projectPath, destinationPath, props, config.required)
+              exec('rm', ['-rf', projectPath])
+            }
           })
-        }
-      })
+        })
+      }
     })
-  }
+  })
 }
 
 function generateApp (source, destinationPath, props, required) {
   const mv = (a, b) => utils.xfs.move(destinationPath(a), destinationPath(b))
   const targetDirName = destinationPath().split('/').pop()
-  utils.xfs.copyTpl(`${source}/**`, destinationPath(), props)
+  console.log('incoming props', props);
+  utils.xfs.copyTpl(`${source}/**/*`, destinationPath(), props)
+  //utils.xfs.copy(`${source}/_generators`, destinationPath('.generators'))
   if (!props.templateRepo || required.indexOf('_choo.yaml') > -1) mv('_choo.yaml', 'choo.yaml')
   if (!props.templateRepo || required.indexOf('_editorconfig') > -1) mv('_editorconfig', '.editorconfig')
   if (!props.templateRepo || required.indexOf('_gitignore') > -1) mv('_gitignore', '.gitignore')
